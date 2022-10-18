ea995c224a94216ec54fa904a98a45d71fb99039
arunthacharuthodi <arunthacharuthodi@gmail.com>
2022-10-18 16:57:13 +0530

    ui done

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -15,42 +15,62 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          child: Column(
-            children: [
-              Row(
-                  children: [
-                    Container(
-                      child: Column(children: [
-                        Text("voltage"),
-                        Text("19V", style: TextStyle(fontSize: 28),),
+        appBar: AppBar(title: Text("WGD CLIENT")),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        child: Column(children: [
+                          Text("voltage"),
+                          Text("19V", style: TextStyle(fontSize: 28),),
 
-                      ]),
-                      
-                    ),
-                    Container(
-                      child: Column(children: [
-                        Text("current"),
-                        Text("1A", style: TextStyle(fontSize: 28),),
+                        ]),
+                        
+                      ),
+                      Container(
+                        child: Column(children: [
+                          Text("current"),
+                          Text("1A", style: TextStyle(fontSize: 28),),
 
-                      ]),
-                      
-                    )
-                  ],
-              ),
-              TextFormField(
-                controller: dutycyclecontroller,
-                decoration: InputDecoration(
-                  hintText: "enter your duty cycle"
+                        ]),
+                        
+                      )
+                    ],
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton( onPressed: () {  },
-                child: Text("upload"),),
-              )
-            ],
-          )
+                Padding(
+                  padding: const EdgeInsets.only(top:30),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom:8.0),
+                        child: TextFormField(
+                          controller: dutycyclecontroller,
+                          decoration: InputDecoration(
+                            border: new OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(8.0),
+                              borderSide: new BorderSide(),
+                            ),
+                            hintText: "Enter your duty cycle"
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton( onPressed: () {  },
+                        child: Text("upload"),),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ),
         ),
       ),
     );
